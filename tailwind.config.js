93ca591d38a920f751b58fa3594518f799b3f21a
IbrahimHassan502 <ih35840@gmail.com>
2024-11-25 10:24:59 +0200

    fixed responsive animation problem

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,11 @@ module.exports = {
       screens: {
         xs: "300px",
       },
+      transitionDuration: {
+        1500: "1500ms",
+        2000: "2000ms",
+        3000: "3000ms",
+      },
     },
   },
   plugins: [],
