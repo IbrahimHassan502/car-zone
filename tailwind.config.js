afc1c361f810388c654b2381eb2365c8bcdc3965
IbrahimHassan502 <ih35840@gmail.com>
2024-11-25 19:20:53 +0200

    fixed hero animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,9 +23,14 @@ module.exports = {
           "0%": { opacity: "0" },
           " 100%": { opacity: "1" },
         },
+        "slide-up-in": {
+          "0%": { opacity: "0", transform: "translateY(66.666%)" },
+          " 100%": { opacity: "1", transform: "translateY(0)" },
+        },
       },
       animation: {
         "fade-in": "fade-in 1.5s ease-out forwards",
+        "slide-up-in": "slide-up-in 1s ease-out forwards",
       },
     },
   },
